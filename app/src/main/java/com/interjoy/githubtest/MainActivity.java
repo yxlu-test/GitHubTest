3395aabe2051c015412366381002baa2a159ea5c
dwang <dwang@interjoy.com.cn>
2017-04-12 18:08:02 +0800

    dwang push hello a !

--- app/src/main/java/com/interjoy/githubtest/MainActivity.java
+++ app/src/main/java/com/interjoy/githubtest/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("TEST", "你好");
-
+        //你好啊
     }
 }
